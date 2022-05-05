2d6f00e0ce4f07ace854e722d8a81257633024d7
Kowalski1024 <tomasz_wojnar@hotmail.com>
2022-05-05 20:55:17 +0200

    create structure of WeatherSkills class

---
+++ src/skills/collection/weather_skills.py
@@ -0,0 +1,14 @@
+
+
+class WeatherSkills:
+    @staticmethod
+    def check_weather():
+        raise NotImplementedError
+
+    @staticmethod
+    def _get_weather_status_and_temperature():
+        raise NotImplementedError
+
+    @staticmethod
+    def _get_city():
+        raise NotImplementedError
