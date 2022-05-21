e1b246781dbc83aa291d42f7b8f44e6d4c547aba
Kowalski1024 <tomasz_wojnar@hotmail.com>
2022-05-21 14:34:43 +0200

    implementation of the Recognizer class

--- src/core/recognizer.py
+++ src/core/recognizer.py
@@ -1,14 +1,30 @@
+import speech_recognition as sr
+from multiprocessing import connection
+
+from src.response import Connection, ResponseType
 
 
-class Recognizer:
-    def __init__(self):
-        raise NotImplementedError
+class Recognizer(Connection):
+    def __init__(self, pipe: connection):
+        super().__init__(pipe)
 
     def run(self):
-        raise NotImplementedError
+        _recognizer = sr.Recognizer()
+        text = self._speech_to_text(_recognizer, self._get_input(_recognizer))
+        self.send(ResponseType.TEXT_RESPONSE, text)
 
-    def _get_input(self):
-        raise NotImplementedError
+    @staticmethod
+    def _get_input(recognizer: sr.Recognizer) -> sr.AudioData:
+        with sr.Microphone() as source:
+            audio = recognizer.listen(source)
+        return audio
 
-    def _speech_to_text(self):
-        raise NotImplementedError
+    @staticmethod
+    def _speech_to_text(recognizer: sr.Recognizer, audio: sr.AudioData) -> str:
+        try:
+            text = recognizer.recognize_google(audio_data=audio)
+        except sr.UnknownValueError:
+            text = "Google Speech Recognition could not understand audio"
+        except sr.RequestError as e:
+            text = f"Could not request results from Google Speech Recognition service; {e}"
+        return text
