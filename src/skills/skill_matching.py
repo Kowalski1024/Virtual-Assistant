2faa88e90c00d2b4417696b98b8cd26f366ccfef
Kowalski1024 <tomasz_wojnar@hotmail.com>
2022-05-22 23:00:33 +0200

    VASSIST-34 feat: implementation of SkillMatching

--- src/skills/skill_matching.py
+++ src/skills/skill_matching.py
@@ -1,13 +1,43 @@
 from multiprocessing import connection
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-class SkillMatching:
-    def __init__(self):
-        raise NotImplementedError
+from .. import ResponseType, Connection
+from ..skills import CMD, assistant_commands
 
-    def run(self, pipe_connection: connection):
-        raise NotImplementedError
+
+class SkillMatching(Connection):
+    def __init__(self, pipe: connection):
+        super().__init__(pipe)
+        self._tfid_vectorizer = TfidfVectorizer()
+        self._skill_matrix = self._tfid_vectorizer.fit_transform([cmd[CMD.TAGS] for cmd in assistant_commands])
+
+    def run(self):
+        skill = None
+        while True:
+            sentence = self.recv_from_speech()
+            match_index, probability = self._find_best_match(sentence)
+
+            if sentence == 'yes' and skill is not None and probability >= 0.5:
+                cls = skill[CMD.CLASS](self._pipe)
+                getattr(cls, skill[CMD.FUNC])()
+                break
+
+            if sentence == 'no':
+                continue
+
+            skill = assistant_commands[match_index]
+            if probability < 0.5:
+                self.send(ResponseType.FAIL_MATCH, f"Sorry no match for \"{sentence}\", please repeat.")
+            elif probability < 1:
+                self.send(ResponseType.FAIL_MATCH, f"Do you mean {skill[CMD.TAGS]}? Say yes/no or repeat.")
+            elif probability == 1:
+                cls = skill[CMD.CLASS](self._pipe)
+                getattr(cls, skill[CMD.FUNC])()
+                break
 
     def _find_best_match(self, sentence):
-        raise NotImplementedError
-
+        similarities = cosine_similarity(self._skill_matrix, self._tfid_vectorizer.transform([sentence])).flatten()
+        best_match_index = similarities.argmax()
+        return best_match_index, similarities[best_match_index].round(2)
