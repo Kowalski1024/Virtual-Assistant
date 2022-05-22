51c4682d6c90e7006ebc847853eeaddfee943f3d
Kowalski1024 <tomasz_wojnar@hotmail.com>
2022-05-22 22:15:41 +0200

    VASSIST-12 feat: wake up the assistant via a keyboard shortcut

--- src/core/assistant.py
+++ src/core/assistant.py
@@ -1,22 +1,41 @@
+import tkinter as tk
+from multiprocessing import Process, Pipe, connection
+
+from .key_listener import KeyListener
+from .recognizer import Recognizer
+from ..gui import GUI
+from ..skills import SkillMatching
+from src.response import Response, ResponseType
 
 
 class Assistant:
     def __init__(self):
-        raise NotImplementedError
+        self._key_listener = KeyListener(self.wake_up)
+        self._pipe_connection = Pipe()
+        self._skill_matching = SkillMatching(self.child_connection)
+        self.skill_matching_process = Process(target=self._skill_matching.run,
+                                              args=(self.child_connection,),
+                                              daemon=True)
 
     def run(self):
         raise NotImplementedError
 
     def wake_up(self):
-        raise NotImplementedError
+        if self.skill_matching_process.is_alive():
+            self.skill_matching_process.terminate()
+        else:
+            self.skill_matching_process = Process(target=self._skill_matching.run,
+                                                  args=(self.child_connection,),
+                                                  daemon=True)
+            self.skill_matching_process.start()
 
     @property
-    def parent_connection(self):
-        raise NotImplementedError
+    def parent_connection(self) -> connection:
+        return self._pipe_connection[0]
 
     @property
-    def child_connection(self):
-        raise NotImplementedError
+    def child_connection(self) -> connection:
+        return self._pipe_connection[1]
 
     def _response(self):
         raise NotImplementedError
---
+++ src/skills/__init__.py
@@ -0,0 +1,1 @@
+from .skill_matching import SkillMatching
