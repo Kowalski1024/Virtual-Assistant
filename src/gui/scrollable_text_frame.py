1675d428f938a682d05e35f3837558316ea05318
Kowalski1024 <tomasz_wojnar@hotmail.com>
2022-05-28 14:31:49 +0200

    VASSIST-8 feat: scrollable text frame

--- src/gui/scrollable_text_frame.py
+++ src/gui/scrollable_text_frame.py
@@ -1,16 +1,27 @@
 import tkinter as tk
 
+from .. import FontStyles
+
 
 class ScrollableTextFrame(tk.Frame):
     def __init__(self, **kw):
         super().__init__(**kw)
-        raise NotImplementedError
+        self.grid_propagate(False)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.txt = tk.Text(self, wrap=tk.WORD)
+        self.txt.grid(row=0, column=0, sticky=tk.NSEW, padx=2, pady=2)
+        scrollb = tk.Scrollbar(self, command=self.txt.yview)
+        scrollb.grid(row=0, column=1, sticky=tk.NSEW)
+        self.txt['yscrollcommand'] = scrollb.set
+        self._prepare_fonts()
 
     def clear_text(self):
-        raise NotImplementedError
+        self.txt.delete(1.0, tk.END)
 
-    def add_text(self, text, font):
-        raise NotImplementedError
+    def add_text(self, text: str, font: FontStyles):
+        self.txt.insert(tk.END, text+"\n", font)
 
     def _prepare_fonts(self):
-        raise NotImplementedError
+        for font in FontStyles:
+            self.txt.tag_configure(font, font=font.value)
