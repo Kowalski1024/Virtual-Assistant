538903587ffc5af03f8eb6717c2f0e50439df0ab
Kowalski1024 <tomasz_wojnar@hotmail.com>
2022-05-28 14:54:07 +0200

    VASSIST-8 feat: GUI and tray icon

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,14 +1,30 @@
+from pystray import Icon, Menu, MenuItem
+from PIL import Image
+from threading import Thread
+import tkinter as tk
+from .response_window import ResponseWindow
+
+
+ICON_PATH = "src/files/icon_circle.ico"
 
 
 class GUI:
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, root: tk.Tk):
+        self.root = root
+        self.response_window = ResponseWindow(self.root)
+        self.tray_icon = Icon('Virtual Assistant', Image.open(ICON_PATH), menu=Menu(MenuItem('Quit', self.on_quit)))
+        self.thread_tray_icon = Thread(target=self.tray_icon.run, daemon=True)
 
     def run(self):
-        raise NotImplementedError
+        self.thread_tray_icon.start()
+        self._prepare_interface()
+        self.root.mainloop()
 
     def on_quit(self):
-        raise NotImplementedError
+        print('Good bye')
+        self.root.destroy()
 
     def _prepare_interface(self):
-        raise NotImplementedError
+        self.root.overrideredirect(True)
+        self.root.geometry("300x200-10-50")
+        self.root.attributes("-topmost", True)
