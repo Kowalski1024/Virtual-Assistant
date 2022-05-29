312f8b8db53b04a8ae7937f8a2e26133bda5cc3b
Kowalski1024 <tomasz_wojnar@hotmail.com>
2022-05-29 14:11:04 +0200

    VASSIST-8 feat: input frame

--- src/gui/input_frame.py
+++ src/gui/input_frame.py
@@ -1,13 +1,23 @@
 import tkinter as tk
 
+from .. import Response, ResponseType
+
 
 class InputFrame(tk.Frame):
-    def __init__(self, **kw):
-        super().__init__(**kw)
-        raise NotImplementedError
+    def __init__(self, pipe, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self._pipe = pipe
+        self.entry = tk.Entry(self)
+        self.button = tk.Button(self, text='Send', command=lambda: self._send(self.entry.get()))
+        self._prepare_frame()
 
-    def _send(self):
-        raise NotImplementedError
+    def _send(self, txt):
+        self._pipe.send(Response(ResponseType.TEXT_RESPONSE, txt))
 
     def _prepare_frame(self):
-        raise NotImplementedError
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(3, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.entry.grid(row=1, column=1, sticky=tk.EW, padx=2, pady=2)
+        self.button.grid(row=2, column=1, sticky=tk.EW, padx=2, pady=2)
