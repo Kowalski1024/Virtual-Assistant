cbfb8126e3def05bc505e934553d2b4b6b7ce8fc
Kowalski1024 <tomasz_wojnar@hotmail.com>
2022-05-29 14:33:01 +0200

    VASSIST-8 chore: GUI add inheritance from Tkinter

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -8,23 +8,23 @@ from .response_window import ResponseWindow
 ICON_PATH = "src/files/icon_circle.ico"
 
 
-class GUI:
-    def __init__(self, root: tk.Tk):
-        self.root = root
-        self.response_window = ResponseWindow(self.root)
+class GUI(tk.Tk):
+    def __init__(self, pipe):
+        super().__init__()
+        self.response_window = ResponseWindow(self, pipe)
         self.tray_icon = Icon('Virtual Assistant', Image.open(ICON_PATH), menu=Menu(MenuItem('Quit', self.on_quit)))
         self.thread_tray_icon = Thread(target=self.tray_icon.run, daemon=True)
 
     def run(self):
         self.thread_tray_icon.start()
         self._prepare_interface()
-        self.root.mainloop()
+        self.mainloop()
 
     def on_quit(self):
         print('Good bye')
         self.root.destroy()
 
     def _prepare_interface(self):
-        self.root.overrideredirect(True)
-        self.root.geometry("300x200-10-50")
-        self.root.attributes("-topmost", True)
+        self.overrideredirect(True)
+        self.geometry("300x200-10-50")
+        self.attributes("-topmost", True)
