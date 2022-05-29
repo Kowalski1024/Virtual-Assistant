1983fec2f7afefbaa6a20a2f75c9785cb9823e71
Kowalski1024 <tomasz_wojnar@hotmail.com>
2022-05-29 14:57:36 +0200

    feat: activating the assistant

--- src/core/assistant.py
+++ src/core/assistant.py
@@ -4,7 +4,7 @@ from multiprocessing import Process, Pipe, connection
 from .key_listener import KeyListener
 from .recognizer import Recognizer
 from ..gui import GUI
-from ..skills import SkillMatching
+from ..skills.skill_matching import SkillMatching
 from src.response import Response, ResponseType
 
 
@@ -12,22 +12,34 @@ class Assistant:
     def __init__(self):
         self._key_listener = KeyListener(self.wake_up)
         self._pipe_connection = Pipe()
+        self._recognizer = Recognizer(self.parent_connection)
         self._skill_matching = SkillMatching(self.child_connection)
-        self.skill_matching_process = Process(target=self._skill_matching.run,
-                                              args=(self.child_connection,),
-                                              daemon=True)
+        self._skill_matching_process = Process()
+        self._speech_recognizer_process = Process()
+        self._graphical_interface = GUI(self.parent_connection)
 
     def run(self):
-        raise NotImplementedError
+        self._key_listener.start()
+        self._graphical_interface.after(ms=20, func=self._response)
+        self._graphical_interface.run()
+        self._key_listener.stop()
+        self.close_connection()
 
     def wake_up(self):
-        if self.skill_matching_process.is_alive():
-            self.skill_matching_process.terminate()
+        if self._skill_matching_process.is_alive():
+            self._skill_matching_process.terminate()
         else:
-            self.skill_matching_process = Process(target=self._skill_matching.run,
-                                                  args=(self.child_connection,),
-                                                  daemon=True)
-            self.skill_matching_process.start()
+            self._skill_matching_process = Process(target=self._skill_matching.run,
+                                                   args=(self.child_connection,),
+                                                   daemon=True)
+            self._skill_matching_process.start()
+
+    def _run_recognizer(self):
+        if not self._speech_recognizer_process.is_alive():
+            self._speech_recognizer_process = Process(target=self._recognizer.run,
+                                                      args=(self.parent_connection,),
+                                                      daemon=True)
+            self._speech_recognizer_process.start()
 
     @property
     def parent_connection(self) -> connection:
@@ -37,5 +49,9 @@ class Assistant:
     def child_connection(self) -> connection:
         return self._pipe_connection[1]
 
+    def close_connection(self):
+        self._pipe_connection[0].close()
+        self._pipe_connection[1].close()
+
     def _response(self):
-        raise NotImplementedError
+        pass
---
+++ src/run.py
@@ -0,0 +1,5 @@
+from src.core.assistant import Assistant
+
+if __name__ == '__main__':
+    assistant = Assistant()
+    assistant.run()
