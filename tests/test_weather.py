578af65fe6e47682a007bf61aa290baea192aede
Arkadiusz Biały <arkadiusz.bialy@onet.pl>
2022-06-01 23:55:23 +0200

    VASSIST-14 fix: fix the weather test to meet the requirements

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -3,6 +3,9 @@ from src.skills.collection.weather_skills import WeatherSkills
 from multiprocessing import Pipe, Process
 from src.response import ResponseType, Response
 
+days = ['Now', 'Today', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+conditions = ['rain', 'sun', 'cloud', 'storm', 'clear', 'hurricane', 'snow', 'tornado', 'wind']
+
 
 class WeatherTests(unittest.TestCase):
     def setUp(self) -> None:
@@ -10,14 +13,37 @@ class WeatherTests(unittest.TestCase):
         self.weather = WeatherSkills(self.child)
         return super().setUp()
 
+    def tearDown(self) -> None:
+        self.parent.close()
+        self.child.close()
+
     def test_weather(self):
         process = Process(target=self.weather.run, daemon=True)
         process.start()
         data: Response = self.parent.recv()
         self.assertIs(ResponseType.WAITING_FOR_SPEECH_INPUT, data.type)
-        self.parent.send(Response(ResponseType.TEXT_RESPONSE, 'Warsaw'))
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, 'Oslo'))
         data: Response = self.parent.recv()
-        self.assertNotEqual(data.message, 'City not found')
+        self.assertEqual(data.message[:data.message.index(',')], 'Oslo')
+        result = data.message.splitlines()
+        i = 0
+        for string in result:
+            if i == 0:
+                i = 1
+                continue
+
+            day = string[:string.index(':')]
+            self.assertIn(day, days)
+            temperature = string[string.index(' ') + 1:string.index('C') - 1]
+            self.assertTrue(temperature.isdigit(), f'{temperature} is not a number')
+            weather_cond = string[string.index('C') + 1:].lower()
+            flag = False
+
+            for condition in conditions:
+                if condition in weather_cond:
+                    flag = True
+
+            self.assertTrue(flag, f'{weather_cond} is not a weather condition')
 
 
 if __name__ == "__main__":
