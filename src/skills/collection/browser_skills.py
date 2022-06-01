630384cd92e7fdd296d6b27cdca7ca704b0e6572
Arkadiusz Biały <arkadiusz.bialy@onet.pl>
2022-06-01 11:27:10 +0200

    VASSIST-5 feat: add show synonyms feature

--- src/skills/collection/browser_skills.py
+++ src/skills/collection/browser_skills.py
@@ -2,6 +2,8 @@ import webbrowser
 
 import wikipedia
 
+import requests
+
 from src.enumerations import FontStyles
 from src.response import ResponseType, Connection
 
@@ -33,9 +35,33 @@ class BrowserSkills(Connection):
         else:
             self.send(ResponseType.SKILL_FAIL, 'Invalid keyword', FontStyles.NORMAL)
 
-    @staticmethod
     def show_synonyms():
-        raise NotImplementedError
+        word = self.recv_from_speech()
+
+        try:
+            url = f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}'
+            response = requests.get(url)
+            data = response.json()
+
+            result = []
+            no_of_words = len(data)
+            
+            # We iterate through the whole API response, because synonyms can be in different places
+            for q in range(no_of_words):
+                no_of_meanings = len(data[q]['meanings'])
+                for i in range(no_of_meanings):
+                    for synonym in data[q]['meanings'][i]['synonyms']:
+                        result.append(synonym) 
+
+                    no_of_definitions = len(data[q]['meanings'][i]['definitions'])
+                    for j in range(no_of_definitions):
+                        for synonym in data[q]['meanings'][i]['definitions'][j]['synonyms']:
+                            result.append(synonym)
+
+            result = str(sorted(set(result))) # avoid repeating synonyms
+            self.send(ResponseType.TEXT_RESPONSE, result, FontStyles.NORMAL)
+        except Exception as e:
+            self.send(ResponseType.TEXT_RESPONSE, 'Cannot find any synonyms', FontStyles.NORMAL)
 
     @staticmethod
     def internet_connectivity_check():
