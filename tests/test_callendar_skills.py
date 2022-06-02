3d081d64aaf040ea9d91bec78a65029c0fe319d7
Arkadiusz Biały <arkadiusz.bialy@onet.pl>
2022-06-02 20:26:47 +0200

    VASSIST-46 test: add test for displaying events

--- tests/test_callendar_skills.py
+++ tests/test_callendar_skills.py
@@ -83,7 +83,76 @@ class CalendarTests(unittest.TestCase):
         data: Response = self.parent.recv()
         self.assertEqual(data.message, 'Duration time is not a number')
         process.terminate()
-        
+
+    def test_displaying_events(self):
+        process = Process(target=self.calendar_skills.display_events, daemon=True)
+        process.start()
+        data: Response = self.parent.recv()
+        self.assertIs(ResponseType.WAITING_FOR_SPEECH_INPUT, data.type)
+        # test for no events
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, '2034-10-10'))
+        data: Response = self.parent.recv()
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, '2034-10-10'))
+        data: Response = self.parent.recv()
+        self.assertEqual(data.message, 'No events found')
+        # test for some events in calendar
+        process.terminate()
+        self.tearDown()
+        self.setUp()
+        process1 = Process(target=self.calendar_skills.create_event, daemon=True)
+        process1.start()
+        # 1st event
+        data: Response = self.parent.recv()
+        self.assertIs(ResponseType.WAITING_FOR_SPEECH_INPUT, data.type)
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, '2022-08-29'))
+        data: Response = self.parent.recv()
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, '20:00'))
+        data: Response = self.parent.recv()
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, 'empty'))
+        data: Response = self.parent.recv()
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, '20'))
+        data: Response = self.parent.recv()
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, 'Cracow'))
+        data: Response = self.parent.recv()
+
+        process1.terminate()
+        self.tearDown()
+        self.setUp()
+
+        process1 = Process(target=self.calendar_skills.create_event, daemon=True)
+        process1.start()
+        data: Response = self.parent.recv()
+        self.assertIs(ResponseType.WAITING_FOR_SPEECH_INPUT, data.type)
+
+        # 2nd event
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, '2022-08-30'))
+        data: Response = self.parent.recv()
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, '21:00'))
+        data: Response = self.parent.recv()
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, 'empty'))
+        data: Response = self.parent.recv()
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, '25'))
+        data: Response = self.parent.recv()
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, 'Cracow'))
+        data: Response = self.parent.recv()
+
+        process1.terminate()
+        self.tearDown()
+        self.setUp()
+
+        # test if events are present
+        process2 = Process(target=self.calendar_skills.display_events, daemon=True)
+        process2.start()
+        data: Response = self.parent.recv()
+        self.assertIs(ResponseType.WAITING_FOR_SPEECH_INPUT, data.type)
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, '2022-08-29'))
+        data: Response = self.parent.recv()
+        self.parent.send(Response(ResponseType.TEXT_RESPONSE, '2022-08-30'))
+        data: Response = self.parent.recv()
+        self.assertNotEqual(data.message, expected_result)
+
+        process2.terminate()
+
 
 if __name__ == "__main__":
     unittest.main()
